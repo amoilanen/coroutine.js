3ed177029fcdf3212cda85fd3d8136207869a591
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-12-01 23:14:32 +0200

    Calls passed function.

--- spec/coroutine.spec.js
+++ spec/coroutine.spec.js
@@ -5,4 +5,19 @@ describe('coroutine', () => {
   it('should be defined', () => {
     expect(coroutine).toBeDefined();
   });
+
+  describe('function argument', () => {
+
+    it('should execute function', () => {
+      var executedFunction = false;
+
+      coroutine(() => {
+        executedFunction = true;
+      });
+      expect(executedFunction).toBe(true);
+    });
+
+    //TODO: Should pass arguments to function
+    //TODO: Should return function return value
+  });
 });
--- src/coroutine.js
+++ src/coroutine.js
@@ -1,4 +1,5 @@
 function coroutine(func) {
+  func();
 }
 
 export {coroutine};
