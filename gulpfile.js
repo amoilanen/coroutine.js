6939b5e274769214767821dd7b62941766b82504
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-01-19 22:24:47 +0200

    Adds the 'clean' task to the build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var clean = require('gulp-clean');
 var Server = require('karma').Server;
 
-gulp.task('default', ['compile', 'test']);
+gulp.task('default', ['clean', 'compile', 'test']);
 
 gulp.task('compile', function() {
   return gulp.src(['src/*.js', 'spec/*.js'])
@@ -11,6 +12,11 @@ gulp.task('compile', function() {
       plugins: ['transform-es2015-modules-umd']
     }))
   .pipe(gulp.dest('compiled'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('compiled/*.js', {read: false})
+    .pipe(clean());
 });
 
 gulp.task('test', ['compile'], function (done) {
